b9fde917914c3beda31403e8c8f5c813b262616d
Muhammad Umair <muhammadumair523@gmail.com>
2023-12-05 16:40:23 +0500

    design updates

--- src/components/modals/post-detail-modal.tsx
+++ src/components/modals/post-detail-modal.tsx
@@ -6,6 +6,7 @@ import { Image } from 'expo-image';
 import { COLORS, FONT_FAMILY, FONT_SIZE } from '@/typography';
 import { Ionicons } from '@expo/vector-icons';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { IMAGES } from '@/constants';
 
 const des = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.';
 
@@ -13,26 +14,31 @@ export const PostDetailModal: React.FC<IPostDetailModalProps> = ({ isVisible, on
   const insets = useSafeAreaInsets();
 
   return (
-    <Modal
-      modalStyle={[styles.modal, { marginTop: insets.top, marginBottom: insets.bottom }]}
-      isVisible={isVisible}
-      onClose={onClose}
-    >
-      <FAB onPress={onClose} style={styles.fab} icon={<Ionicons name='close' size={24} color={COLORS.WHITE} />} />
-      <Image
-        source={{ uri: selectedPost?.imageURL }}
-        contentFit='cover'
-        placeholder={'https://placehold.co/400'}
-        placeholderContentFit='cover'
-        style={styles.image}
-        cachePolicy={'memory-disk'}
-      />
-      <View style={styles.modalContent}>
-        <View style={styles.userRow}>
-          <Image source={{ uri: selectedPost?.user?.profilePicURL }} style={styles.userImage} />
-          <Text style={styles.userName}>{selectedPost?.user?.name}</Text>
+    <Modal modalStyle={styles.modal} isVisible={isVisible} onClose={onClose} animationType='slide'>
+      <View style={[styles.root, { marginTop: insets.top, marginBottom: insets.bottom }]}>
+        <FAB onPress={onClose} style={styles.fab} icon={<Ionicons name='close' size={24} color={COLORS.WHITE} />} />
+        <Image
+          source={{ uri: selectedPost?.imageURL }}
+          contentFit='cover'
+          placeholder={'https://placehold.co/400'}
+          placeholderContentFit='cover'
+          style={styles.image}
+          cachePolicy={'memory'}
+        />
+        <View style={styles.modalContent}>
+          <View style={styles.userRow}>
+            <Image
+              source={{ uri: selectedPost?.user?.profilePicURL }}
+              contentFit='cover'
+              cachePolicy={'memory'}
+              placeholder={IMAGES.USER}
+              placeholderContentFit='cover'
+              style={styles.userImage}
+            />
+            <Text style={styles.userName}>{selectedPost?.user?.name}</Text>
+          </View>
+          <Text style={styles.description}>{selectedPost?.content ?? des}</Text>
         </View>
-        <Text style={styles.description}>{selectedPost?.content ?? des}</Text>
       </View>
     </Modal>
   );
@@ -41,8 +47,11 @@ export const PostDetailModal: React.FC<IPostDetailModalProps> = ({ isVisible, on
 const styles = StyleSheet.create({
   modal: {
     flex: 1,
-    justifyContent: 'center',
     backgroundColor: COLORS.BLACK,
+  },
+  root: {
+    flex: 1,
+    justifyContent: 'center',
   },
   modalContent: {
     position: 'absolute',
